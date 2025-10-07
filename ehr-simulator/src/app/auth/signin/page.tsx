5cb51d04dd9d8c553c518908bd55fae9b1709f71
Max Mulder <maxmulder03@gmail.com>
2025-10-07 17:49:25 -0400

    init sign in UI

--- ehr-simulator/src/app/auth/signin/page.tsx
+++ ehr-simulator/src/app/auth/signin/page.tsx
@@ -1,3 +1,66 @@
+'use client';
+
+import { useState } from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 export default function SignIn() {
-  return <h1>Sign In</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full gap-y-16">
+      <div>
+        <h1 className="text-6xl font-bold tracking-tight text-center">
+          EHR Simulator
+        </h1>
+        <h2 className="text-lg italic text-muted-foreground text-center">presented by GVSU</h2>
+      </div>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Sign In</CardTitle>
+          <CardDescription>Use your GVSU email to sign in</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">
+            Login with Google
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
