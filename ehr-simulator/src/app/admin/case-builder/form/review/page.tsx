85035cf495048e2a42d130a68ad7cb7e167be38c
Matt Smith <mattsmith1652@gmail.com>
2025-12-10 13:50:35 -0500

    pretty printing JSON in review page

--- ehr-simulator/src/app/admin/case-builder/form/review/page.tsx
+++ ehr-simulator/src/app/admin/case-builder/form/review/page.tsx
@@ -4,8 +4,8 @@ import { useFormContext } from "@/context/FormContext"
 const FormReview = () => {
   const { data } = useFormContext()
   return (
-    <div className="w-full h-screen text-wrap p-4">
-      <p className="">{JSON.stringify(data, null, 2)}</p>
+    <div className="w-full h-screen p-4">
+      <pre id='JSON'>{JSON.stringify(data, null, 4)}</pre>
     </div>
   )
 }
