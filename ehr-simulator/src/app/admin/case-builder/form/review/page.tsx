46205db12ce69fa6a9ad03aec02592dda4abe9a0
Zachary Williams <zacharywilliams080802@gmail.com>
2026-01-14 14:48:10 -0500

    prettier review page

--- ehr-simulator/src/app/admin/case-builder/form/review/page.tsx
+++ ehr-simulator/src/app/admin/case-builder/form/review/page.tsx
@@ -1,11 +1,20 @@
 'use client'
+import { Card } from "@/components/ui/card"
 import { useFormContext } from "@/context/FormContext"
 
 const FormReview = () => {
-  const { data } = useFormContext()
+  const { demographicData, historyData, noteData, orderData, ioData, labData, chartingData, medOrderData, medAdministrationData } = useFormContext()
+  const titles = ["Demographics", "History", "Notes", "Orders", "Intake/Output", "Labs", "Charting", "Med Orders", "Med Administrations"]
   return (
     <div className="w-full h-screen p-4">
-      <pre id='JSON'>{JSON.stringify(data, null, 4)}</pre>
+      {[demographicData, historyData, noteData, orderData, ioData, labData, chartingData, medOrderData, medAdministrationData].map((data, index) => (
+        <Card key={index} className="my-6 pl-3 text-sm">
+          <div>
+            <h1 className="text-lg font-bold ml-3">{titles[index]} Data</h1>
+            <pre id='JSON'>{JSON.stringify(data, null, 4)}</pre>
+          </div>
+        </Card>
+      ))}
     </div>
   )
 }
